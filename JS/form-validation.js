6c2a82e05f3b0dae43e367e922366970e49fcc1d
agustin-schiaffino <schiaffinoagustin95@gmail.com>
2021-10-15 15:38:55 -0300

    full name validation in progress

--- JS/form-validation.js
+++ JS/form-validation.js
@@ -1,6 +1,26 @@
 window.onload=function() {
     //Full name validation
-    var fullName = document.getElementById('name');
-    var fullNameError = document.getElementById('name-error');
+    var fullName = document.getElementById("name");
+    var fullNameError = document.getElementById("name-error");
+    fullName.addEventListener("blur", validateName);
+    function validateName() {
+        var space = fullName.value.indexOf(" ");
+        if (space<=0 || fullName.value.lenght<6 || fullName.value==null) {
+            fullNameError.style.display="block";
+            fullNameError.style.fontSize="10px";
+            fullName.style.border="2px solid red";
+        }
+        else {
+            fullName.style.border= "2px solid green"
+        }
+    }
+    fullName.addEventListener("focus", focusValidation)
+    function focusValidation() {
+        fullNameError.style.display="none"
+        
+    }
+
+    //Email Validation
+
 }
     
